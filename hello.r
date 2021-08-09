fa7037cdcfed5cab0c8f251e63b42ef703eaa72e
Kristinnatalia <christinnathalia74@gmail.com>
2021-08-09 11:39:50 +0800

    cek

--- hello.r
+++ hello.r
@@ -1,5 +1,5 @@
-teks1 = "Selamat datang di dunia R!"
-teks2 = "Adnda akan terkesima dengan kehebatannya!"
+teks1 = "Selamat datang di dunia R!!"
+teks2 = "Anda akan terkesima dengan kehebatannya!!"
 
 teks1
 teks2
